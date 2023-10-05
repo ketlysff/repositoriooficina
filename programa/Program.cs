d140e15d677aae989027b5e18494309eaef4b89b
ketlysff <ketlyfreire21@gmail.com>
2023-10-05 15:47:28 -0400

    sim

--- programa/Program.cs
+++ programa/Program.cs
@@ -9,6 +9,15 @@ class Program
         Console.WriteLine("DIGITE OUTRO NÚMERO:");
         double numero2 = Convert.ToDouble(Console.ReadLine());
 
+        if (numero2 > numero)
+        {
+            Console.WriteLine($"O numero {numero2} é maior");
+        }
+        else
+        {
+            Console.WriteLine($"O numero {numero} é maior");
+        }
+
         Console.WriteLine($"A soma dos dois números é: {numero + numero2}");
 
         Console.ReadKey();
